a0416111d462a319c1567f161f1095f6214eadd3
Anastasiia <nastiksot02@gmail.com>
2022-01-30 14:28:06 -0800

    Video 2 | Parallel spawn and movement

--- Assets/Section2/Video 1/BadSpawn.cs
+++ Assets/Section2/Video 1/BadSpawn.cs
@@ -1,17 +1,20 @@
 using UnityEngine;
 
-public class BadSpawn : MonoBehaviour
+namespace Section2.Video_1
 {
-    [SerializeField] private GameObject sheepPrefab;
-    [SerializeField] private int spawnSheepCount; 
-    
-    void Start()
+    public class BadSpawn : MonoBehaviour
     {
-        for (int i = 0; i < spawnSheepCount; i++)
+        [SerializeField] private GameObject sheepPrefab;
+        [SerializeField] private int spawnSheepCount; 
+    
+        void Start()
         {
-            var sheepPosition = new Vector3(Random.Range(-50, 50), 0, Random.Range(-50, 50));
-            Instantiate(sheepPrefab, sheepPosition, Quaternion.identity);
+            for (int i = 0; i < spawnSheepCount; i++)
+            {
+                var sheepPosition = new Vector3(Random.Range(-50, 50), 0, Random.Range(-50, 50));
+                Instantiate(sheepPrefab, sheepPosition, Quaternion.identity);
+            }
         }
-    }
  
+    }
 }
---
+++ Assets/Section2/Video 2/BadSpawnAndMoveParallel.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Jobs;
+using Unity.Jobs;
+
+namespace Section2.Video_2
+{
+    public class BadSpawnAndMoveParallel : MonoBehaviour
+    {
+        [SerializeField] private GameObject sheepPrefab;
+        [SerializeField] private int spawnSheepCount;
+        private List<GameObject> instantiatedSheeps = new List<GameObject>();
+
+        void Start()
+        {
+            for (var i = 0; i < spawnSheepCount; i++)
+            {
+                var sheepPosition = new Vector3(Random.Range(-50, 50), 0, Random.Range(-50, 50));
+                var instantiatedSheep = Instantiate(sheepPrefab, sheepPosition, Quaternion.identity);
+                instantiatedSheeps.Add(instantiatedSheep);
+            }
+        }
+
+        void Update()
+        {
+            foreach (var instantiatedSheep in instantiatedSheeps)
+            {
+                instantiatedSheep.transform.Translate(0f, 0f, 0.1f);
+                if (instantiatedSheep.transform.position.z < 50) return;
+
+                var sheepPosition = instantiatedSheep.transform.position;
+                var newPosition = new Vector3(sheepPosition.x, sheepPosition.y, -50f);
+                
+                sheepPosition = newPosition;
+                instantiatedSheep.transform.position = sheepPosition;
+            }
+        }
+    }
+}
