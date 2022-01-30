1c8db1694fd39d0db81031ce13ae7684695f6d18
Anastasiia <nastiksot02@gmail.com>
2022-01-30 15:17:56 -0800

    Video 4 | Spawn with ECS manager

---
+++ Assets/Section2/Video 4/ECSManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Transactions;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+public class ECSManager : MonoBehaviour
+{
+    [SerializeField] private GameObject sheepPrefab;
+    [SerializeField] private int spawnSheepCount;
+
+    private EntityManager entityManager;
+
+    void Start()
+    {
+        entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+        var settings = GameObjectConversionSettings.FromWorld(World.DefaultGameObjectInjectionWorld, null);
+        var ecsPrefab = GameObjectConversionUtility.ConvertGameObjectHierarchy(sheepPrefab, settings);
+
+        for (int i = 0; i < spawnSheepCount; i++)
+        {
+            var instance = entityManager.Instantiate(ecsPrefab);
+            var position = transform
+                .TransformPoint(
+                    new float3(UnityEngine.Random.Range(-50, 50), 0, UnityEngine.Random.Range(-50, 50)));
+            entityManager.SetComponentData(instance, new Translation { Value = position, });
+            entityManager.SetComponentData(instance, new Rotation { Value = new quaternion(0,0,0,0), });
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
