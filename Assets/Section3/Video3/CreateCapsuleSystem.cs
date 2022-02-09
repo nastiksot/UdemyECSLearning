2808f1f7c5162d316081262eb01d4bb38b49439d
Anastasiia <nastiksot02@gmail.com>
2022-02-09 13:03:45 -0800

    Video 7 | Constructing an Entity from Scratch

---
+++ Assets/Section3/Video3/CreateCapsuleSystem.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Section3.Video3;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using Unity.Rendering;
+using Unity.Jobs;
+
+public class CreateCapsuleSystem : JobComponentSystem
+{
+    protected override void OnCreate()
+    {
+        base.OnCreate();
+
+        var instance = EntityManager.CreateEntity(
+            ComponentType.ReadOnly<LocalToWorld>(),
+            ComponentType.ReadOnly<RenderBounds>(),
+            ComponentType.ReadOnly<RenderMesh>()
+        );
+
+        EntityManager.SetComponentData(instance,
+            new RenderBounds
+            {
+                Value = new AABB()
+            });
+
+        EntityManager.SetComponentData(instance,
+            new LocalToWorld
+            {
+                Value = new float4x4(quaternion.identity, new float3(0, 0, 0))
+            });
+
+        var resourceHolder = Resources.Load<GameObject>("ResourceHolder").GetComponent<ResourceHolder>();
+
+        EntityManager.SetSharedComponentData(instance,
+            new RenderMesh
+            {
+                mesh = resourceHolder.Mesh,
+                material = resourceHolder.Material
+            });
+    }
+
+
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        return inputDeps;
+    }
+}
---
+++ Assets/Section3/Video3/ResourceHolder.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Section3.Video3
+{
+    public class ResourceHolder : MonoBehaviour
+    {
+        [SerializeField] private Mesh mesh;
+        [SerializeField] private Material material;
+
+        public Mesh Mesh => mesh;
+        public Material Material => material;
+    }
+}
