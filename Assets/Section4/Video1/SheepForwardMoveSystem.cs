ddeb0cdca91df4bae7c09473e43b0e75f9c5c1e8
Anastasiia <nastiksot02@gmail.com>
2022-02-09 15:31:26 -0800

    Video 2 | Performing Rotations

--- Assets/Section4/Video1/SheepForwardMoveSystem.cs
+++ Assets/Section4/Video1/SheepForwardMoveSystem.cs
@@ -11,11 +11,14 @@ namespace Section4.Video1
         protected override JobHandle OnUpdate(JobHandle inputDeps)
         {
             var deltaTime = Time.DeltaTime;
-            var forwardPosition = new float3(5f, 0f, 5f);
+            var forwardPosition = new float3(5f, 5f, 5f);
             var jobHandle = Entities.WithName("SheepForwardMoveSystem")
                 .ForEach((ref Translation position, ref Rotation rotation, ref SheepData palmTreeData) =>
                 {
-                    position.Value += deltaTime * 0.5f * (forwardPosition - position.Value);
+                    var targetRotation = forwardPosition - position.Value;
+                    targetRotation.y = 0;
+                    rotation.Value = quaternion.LookRotation(targetRotation, math.up());
+                    position.Value += deltaTime * 0.7f * math.forward(rotation.Value);
                 })
                 .Schedule(inputDeps);
             return jobHandle;
